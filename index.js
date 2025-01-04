42c41e1531f9fb59f58a03dfd5d6b2302eecd559
Arshakh Haneen <arshx77@gmail.com>
2025-01-04 22:28:38 +0530

    Exercise 3.6

--- index.js
+++ index.js
@@ -56,17 +56,31 @@ app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
   persons = persons.filter((person) => id !== person.id);
   console.log(persons);
-  res.status(204).end()
+  res.status(204).end();
 });
 
-app.post("/api/persons/",(req,res) => {
+app.post("/api/persons/", (req, res) => {
   const person = req.body;
- 
-  let id = Math.floor(Math.random()*1000000);
-  person.id = String(id);
-  persons = persons.concat(person);
-  res.json(persons)
-})
+
+  if (!person.name) {
+    return res.status(400).json({ error: "Name is missing" });
+  }
+
+  if (!person.number) {
+    return res.status(400).json({ error: "Number is missing" });
+  }
+
+  const nameExists = persons.find((p) => p.name === person.name);
+  if (nameExists) {
+    return res.status(400).json({ error: "Name must be unique" });
+  }
+
+  let id = Math.floor(Math.random() * 1000000);
+  const newPerson = { ...person, id: String(id) };
+  persons = persons.concat(newPerson);
+
+  res.status(201).json(newPerson);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
