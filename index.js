65b2ce735d1ccb64da48171c1b0f8d7245ac9e24
Arshakh Haneen <arshx77@gmail.com>
2025-01-06 13:35:30 +0530

    Exercise 3.7

--- index.js
+++ index.js
@@ -28,8 +28,11 @@ function getCurrentTime() {
 }
 
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hellllooooo</<h1>");
